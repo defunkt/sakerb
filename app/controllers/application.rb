82ecf472b630e18f09120a1a1c73b224f67de044
Chris Wanstrath <chris@ozmm.org>
2008-03-19 10:39:02 -0400

    put authenticated system into the top level

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,6 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  include AuthenticatedSystem
+  helper :all 
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '11493451c50ebd1f5ee9aed5a55b6fe5'
+  protect_from_forgery 
 end
